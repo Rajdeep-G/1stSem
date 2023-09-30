37c50723841d29f08ec4f44052738faaea9e85e4
Rajdeep Ghosh <rajdeepghosh@Rajdeeps-MacBook-Air.local>
2023-09-30 13:28:40 +0530

    upd

--- CL lab/LP/moodle/23CS60R10_Q2.py
+++ CL lab/LP/moodle/23CS60R10_Q2.py
@@ -3,27 +3,27 @@ from ortools.linear_solver import pywraplp
 
 def optimize_production(no_of_pdt,no_of_res, profits, resource_availability, resource_usage,max_sales):
 
-    solver = pywraplp.Solver.CreateSolver('GLOP') #can make it SCIP
+    solver = pywraplp.Solver.CreateSolver('GLOP')
 
     production_plan = []
     for i in range(no_of_pdt):
-        production_plan.append(solver.NumVar(1, max_sales[i], f'var_{i}')) #make it IntVar 
+        production_plan.append(solver.IntVar(0, solver.infinity(), f'var_{i}'))
 
-    print(production_plan)
+    for i in range(no_of_pdt):
+        solver.Add(production_plan[i] <= max_sales[i])
 
     for j in range(no_of_res):
         total_resource_usage = solver.Sum(production_plan[i] * resource_usage[i][j] for i in range(no_of_pdt))
         solver.Add(total_resource_usage <= resource_availability[j])
-    
 
-    
+      
+
     objective = solver.Objective()
-    
+
     for i in range(no_of_pdt):
         objective.SetCoefficient(production_plan[i], profits[i])
     objective.SetMaximization()
 
-    solver.Solve() #added this
 
 
     with open('output_Q2.txt', 'w') as file:
@@ -34,6 +34,16 @@ def optimize_production(no_of_pdt,no_of_res, profits, resource_availability, res
 
 
 
+# no_of_pdt=(int)(input())
+# no_of_res=(int)(input())
+# profits = list(map(int,input().split()))
+# res_avail = list(map(int,input().split()))
+
+
+# all_res=[]
+# for i in range(no_of_pdt):
+#     each_res=list(map(int,input().split()))
+#     all_res.append(each_res)
 
 with open('input_Q2.txt', 'r') as file:
     no_of_pdt = int(file.readline())

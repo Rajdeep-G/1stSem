875fd76b20fe798d488b60dde5751ec9eb78c956
Rajdeep Ghosh <rajdeepghosh@Rajdeeps-MacBook-Air.local>
2023-10-10 13:50:21 +0530

    upto D all done

--- OS/ASSignment7/D/my_shell.c
+++ OS/ASSignment7/D/my_shell.c
@@ -8,14 +8,145 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+int remove_last_amp(char *user_input)
+{
+    int bg_process_flag = 0;
+    if (user_input[strlen(user_input) - 1] == '&')
+    {
+        bg_process_flag = 1;
+        user_input[strlen(user_input) - 1] = '\0';
+        if (strcmp(user_input, "") == 0)
+            user_input[strlen(user_input) - 1] = '\0';
+    }
+    return bg_process_flag;
+}
+
+int execute_piped_command(char *command)
+{
+    char *input[1000];
+    int count = 0;
+    char *token = strtok(command, "|");
+    while (token != NULL)
+    {
+        input[count++] = token;
+        token = strtok(NULL, "|");
+    }
+    input[count] = NULL;
+
+    // check for the last command
+    char *last_command = input[count - 1];
+
+    int bg_process_flag = 0;
+
+    // Create an array to store file descriptors for pipes
+    int pipe_fds[count - 1][2];
+
+    for (int i = 0; i < count; i++)
+    {
+        // Trim leading and trailing spaces from each command
+        char *cmd = input[i];
+        while (*cmd == ' ')
+            cmd++;
+        int len = strlen(cmd);
+        while (len > 0 && cmd[len - 1] == ' ')
+            cmd[--len] = '\0';
+        // ...............................................................
+        // Tokenize the command into arguments
+        char *parameter[1000];
+        int no_param = 0;
+        token = strtok(cmd, " ");
+        while (token != NULL)
+        {
+            parameter[no_param++] = token;
+            token = strtok(NULL, " ");
+        }
+        parameter[no_param] = NULL;
+        // ...............................................................
+
+        int bg_process_flag = 0;
+        if (i == count - 1)
+            bg_process_flag = remove_last_amp(parameter[no_param - 1]);
+
+        if (i < count - 1)
+        {
+            // If not the last command, create all pipes
+            if (pipe(pipe_fds[i]) == -1)
+            {
+                perror("Pipe failed");
+                exit(1);
+            }
+        }
+
+        pid_t child_pid;
+        child_pid = fork(); // Create a child process
+        if (child_pid == -1)
+        {
+            perror("Fork failed");
+            exit(1);
+        }
+        if (child_pid == 0)
+        {
+            if (i > 0)
+            {
+                // Connect stdin to the read end of the previous pipe
+                dup2(pipe_fds[i - 1][0], STDIN_FILENO);
+                close(pipe_fds[i - 1][0]);
+            }
+            if (i < count - 1)
+            {
+                // Connect stdout to the write end of the current pipe
+                dup2(pipe_fds[i][1], STDOUT_FILENO);
+                close(pipe_fds[i][1]);
+            }
+            // Execute the command
+            if (strcmp(parameter[0], "cd") == 0)
+            {
+                if (chdir(parameter[1]) == -1)
+                {
+                    perror("chdir failed");
+                    exit(1);
+                }
+            }
+            else if (execvp(parameter[0], parameter) == -1)
+            {
+                perror("execvp failed");
+                exit(1);
+            }
+        }
+        else
+        {
+            // Close the write end of the current pipe (if not the last command)
+            if (i < count - 1)
+            {
+                close(pipe_fds[i][1]);
+            }
+            // Wait for the child process
+            if (bg_process_flag != 1)
+            {
+                int status;
+                waitpid(child_pid, &status, 0);
+            }
+        }
+    }
+
+    // Close all pipe file descriptors
+    for (int i = 0; i < count - 1; i++)
+    {
+        close(pipe_fds[i][0]);
+        close(pipe_fds[i][1]);
+    }
+
+    return 0;
+}
+
 int execute_command(char *command)
 {
 
     if (strstr(command, "|"))
     {
         // If the command contains a pipe character, execute as piped command
-        // execute_piped_command(command);
-        printf("Piped commands not supported yet\n");
+        execute_piped_command(command);
+        // printf("Piped commands not supported yet\n");
     }
     else
     {
@@ -31,16 +162,8 @@ int execute_command(char *command)
         input[count] = NULL;
         int bg_process_flag = 0;
 
-        if (input[count - 1][strlen(input[count - 1]) - 1] == '&')
-        {
-            bg_process_flag = 1;
-            input[count - 1][strlen(input[count - 1]) - 1] = '\0';
-            if (strcmp(input[count - 1], "") == 0)
-            {
-                count -= 1;
-                input[count] = NULL;
-            }
-        }
+        if (count > 1)
+            bg_process_flag = remove_last_amp(input[count - 1]);
 
         if (strcmp(input[0], "vi") == 0)
         {
@@ -144,14 +267,6 @@ int main()
         {
 
             add_history(user_input); // Add the input to the command history.
-
-            // if (strncmp(user_input, "vi ", 3) == 0)
-            // {
-            //     // User wants to open Vi editor with a file, e.g., "vi filename"
-            //     char *filename = user_input + 3; // Extract filename
-            //     // Launch the Vi editor to edit the specified file
-            //     vi_editor(filename);
-            // }
             if (strcmp(user_input, "exit") == 0)
             {
                 free(user_input);
